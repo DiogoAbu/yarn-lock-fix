dba5a9e39b4498b56f8e4082efbcb606e24b9de7
Diogo de Azevedo Silva <diogodeazevedosilva@gmail.com>
2021-04-05 16:39:40 -0300

    feat: :tada: initial commit [skip ci]

---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+(async () => {
+  let files;
+  try {
+    const { stdout } = await exec('git diff --staged --name-only');
+    files = stdout;
+  } catch (err) {
+    console.error('Could not get staged files');
+    process.exit(5);
+  }
+
+  if (!files.includes('package.json')) {
+    console.info('Nothing changed on package.json');
+    process.exit(0);
+  }
+
+  try {
+    await exec('yarn install --frozen-lockfile');
+    console.info('Lockfile is up-to-date or does not exists');
+    process.exit(0);
+  } catch (err) {
+    if (!err.stderr.includes('lockfile needs to be updated')) {
+      console.error('Could not check lockfile');
+      process.exit(5);
+    }
+  }
+
+  try {
+    console.log('Updating lockfile');
+    await exec('yarn install --non-interactive');
+    await exec('git add yarn.lock');
+    console.log('Successfully updated and staged lockfile');
+    process.exit(0);
+  } catch (err) {
+    console.error('Could not update lockfile');
+    process.exit(5);
+  }
+})();
